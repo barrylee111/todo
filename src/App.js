7d245f993877ffdbc5e900d20eb273b9c3650d55
Barry Lee <barry.lee@solertium.com>
2022-04-14 14:46:20 -0600

    Set requirements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div style={{ marginLeft: '30px' }}>
+    <h2>Build a Todo List</h2>
+    <h4>Acceptance Criteria</h4>
+    <li>Add a title for the list</li>
+    <li>User should be able to input new items</li>
+    <li>Items should be added to list with checkbox</li>
+    <li>Default state of items should be unchecked</li>
+    <li>State should update on check</li>
+    <br />
+    <li>Bonus: If all items are checked, Title should change (strikethrough, checkbox, color, etc)</li>
+
+  </div>
+)
 
 export default App;
